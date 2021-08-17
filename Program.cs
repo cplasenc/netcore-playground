2112e9a60160f4550d4754ed3ba5e8c06b9133c2
Cristian Plasencia <plc.cristian@gmail.com>
2021-08-17 17:48:17 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Http;
+using System;
+
+namespace Demo_launch_app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var hostBuilder = Host.CreateDefaultBuilder(args)
+                                    .ConfigureWebHostDefaults(b =>
+                                    {
+                                        b.UseStartup<Program>();
+                                    });
+            var host = hostBuilder.Build();
+            host.Run();
+        }
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.Run(async context =>
+            {
+                await context.Response.WriteAsync($"Hi! We are in {env.EnvironmentName}");
+            });
+        }
+    }
+}
